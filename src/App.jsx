5d92094a87f1745fddd146362f0681a023f20ca8
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-10-27 22:49:00 +0200

    defined the Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import Home from './pages/Home.jsx'
-
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Menu from './pages/Menu.jsx';
+import Contact from './pages/Contacts.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Header />  
-      <main className="container mx-auto p-4">  
-       <Home />
-      </main> 
+    <Router>
+      <Header />
+
+      <main className="container mx-auto p-4 min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+
       <Footer />
-  
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
